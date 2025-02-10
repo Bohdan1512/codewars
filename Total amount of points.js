69781e7796b95f29e712d1d0d6ba9656dbc78e7d
Bohdan1512 <pobaranchukbbb@gmail.com>
2025-02-10 18:59:47 +0100

    new kata

--- Total amount of points.js
+++ Total amount of points.js
@@ -1,5 +1,19 @@
 function points(games) {
+    let totalPoints = 0;
 
+    for (let game of games) {
+        let [x, y] = game.split(':'); // Разделяем строку на два значения x и y
+
+        if (x > y) {
+            totalPoints += 3; // Победа
+        } else if (x < y) {
+            totalPoints += 0; // Проигрыш
+        } else {
+            totalPoints += 1; // Ничья
+        }
+    }
+
+    return totalPoints;
 }
 
 
