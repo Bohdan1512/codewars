ed686f75bfda4e296a9643aa15122a9cf0e61769
Bohdan1512 <pobaranchukbbb@gmail.com>
2025-03-11 16:25:52 +0100

    new kata

--- 8 kyu Counting sheep....js
+++ 8 kyu Counting sheep....js
@@ -1,8 +1,4 @@
 function countSheeps(sheep) {
-    if(i => i == true && i == false) {
         return sheep.filter(i => i === true).length
-    } else {
-        return "error";
-    }
 }
 console.log(countSheeps(['true','true','false','true','false','true','true','false','false','false','true']));
